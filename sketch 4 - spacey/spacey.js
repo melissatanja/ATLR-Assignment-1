1ba0c7865f77563e40b7d245651ec69751f40d9d
melissatanja <melissatanja02@users.noreply.github.com>
2018-09-13 18:41:28 -0400

    added herder assets

--- sketch 4 - spacey/spacey.js
+++ sketch 4 - spacey/spacey.js
@@ -1,29 +1,25 @@
-// var textures;
-// //var t = random(textures[i]);
+var textures;
 
 // var t; //= random(0, textures.length);
 // //textures[t];
 
 var i1;
 
-// function preload(){
-// 	i1 = loadImage('assets/rock1.jpg');
-// 	textures = i1;
-// }
-
-// textures = append(textures, i1);
+function preload(){
+	i1 = loadImage('assets/rock1.jpg');
+	textures = i1;
+}
 
 var r = 0;
+var textures = [];
 
 function setup(){
 	createCanvas(800, 800, WEBGL);
+	var t = random(0, textures.length);
 }
 
 function draw(){
 	background(0, 37, 60);
-
-	var dirX = (mouseX / width - 0.5) *2;
-	var dirY = (mouseY / height - 0.5) *2;
 
 	pointLight(255, 247, 221, 0, 0, 0);
 	pointLight(255, 247, 221, 0, 0, 0);//double the power
@@ -46,17 +42,11 @@ function draw(){
 	r+=0.005;
 }
 
-// function planet(s, r, t, x, y, z){
-// 	translate(x, y, z);
-// 	sphere(s);
-// 	rotateY(); 
-	//texture(textures[t]);
-// }
-
-function planet(s, r, x, y, z){
+function planet(s, r, t, x, y, z){
 	push();
 	translate(x,y,z);
 	rotateY(r);
+	texture(textures[t]);
 	sphere(s); 
 	translate(-x, -y, -z);
 	pop();
